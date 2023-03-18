d451ce0079f8d27397cd03cd48f7031bdb1406aa
amit1366 <69159563+amit1366@users.noreply.github.com>
2023-03-19 03:09:39 +0530

    update

--- 16574-Brillmark/bl1ca0hcfh-brillmark.com/experiments/190725-heading_change/833066-Variation1.js
+++ 16574-Brillmark/bl1ca0hcfh-brillmark.com/experiments/190725-heading_change/833066-Variation1.js
@@ -1,1 +1,17 @@
-console.log("heading changes 1 hello");
+console.log("heading changes 1 hello Amit Bisht");
+var kamleaoonel = Kameleoon.API.Utils
+var abc = kamleaoonel.querySelectorAll('.animation-header')[0]
+console.log(abc);
+// get text other page
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var parse = new DOMParser();
+        var doc = parse.parseFromString(this.response, 'text/html')
+        console.log(doc)
+        var docel = doc.querySelector('.header')
+        console.log(docel)
+    }
+};
+xhttp.open("GET", "https://www.congstar.de/", true);
+xhttp.send()
