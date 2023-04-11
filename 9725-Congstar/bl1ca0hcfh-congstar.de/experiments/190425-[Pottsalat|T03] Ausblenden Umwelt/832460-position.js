b6f146a3d8a5df6148acb9f3dee525202f5d9cf2
amit1366 <69159563+amit1366@users.noreply.github.com>
2023-04-11 15:54:54 +0530

    update

--- 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190425-[Pottsalat|T03] Ausblenden Umwelt/832460-position.js
+++
@@ -1,8 +0,0 @@
-function init() {
-    console.log('hello');
-    Kameleoon.API.Utils.querySelectorAll('body')[0].classList.add('bmkameleoon-Pottsalat-T03')
-}
-
-Kameleoon.API.Core.runWhenElementPresent("body", function () {
-    init()
-}, 200) 
--- 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190425-[Pottsalat|T03] Ausblenden Umwelt/selectors.js
+++
@@ -1,8 +0,0 @@
-export const selectors = {
-    accordionTab: '.bm_accordion_item',
-    accordionShow: 'bm-accordianactive',
-    accordianExist: '.bm-accordianactive',
-    stromPageEL: '.trust-area',
-    gasPageEl: '#main-content .new-flex-row',
-    bodyClsel: 'bm-accordian'
-};
