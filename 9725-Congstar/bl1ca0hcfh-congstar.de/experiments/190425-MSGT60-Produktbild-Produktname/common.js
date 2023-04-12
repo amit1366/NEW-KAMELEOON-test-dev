96fcae177aa0daffbdb4c07298417bdbd9f66717
Hariom-BM <96159244+Hariom-BM@users.noreply.github.com>
2023-04-12 10:52:52 +0530

    produktname folder created

---
+++ 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190425-MSGT60-Produktbild-Produktname/common.js
@@ -0,0 +1,17 @@
+// import { goals } from './goals';
+// import { selectors } from './selectors';
+
+// export const commongoals = () => {
+//     Kameleoon.API.Core.runWhenElementPresent('#main-content', () => {
+//     // console.log(' inside main function - common code');
+//     Kameleoon.API.Utils.addUniversalClickListener(document, ({ target }) => {
+//         let accordianTab = target.closest(selectors.accordionTab);
+//         if (accordianTab) {
+//             console.log('click goal');
+//         }
+//     });
+
+
+// }, 200);
+
+// }
---
+++ 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190425-MSGT60-Produktbild-Produktname/html.js
@@ -0,0 +1,192 @@
+
+
+// energie and bonus 
+export const bmstring = `
+<div class="bm-side-nav">
+<ul class="bm-side-nav-inner">
+    <li class="bm-side-nav-content">
+
+        <div class="bm-nav-item-wrapper">
+            <ul>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Befestigungs-Montagetechnik/1601AC02_3501244.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_02">
+                        <i data-cyid="cyid3501244" class="icon-cyid3501244"></i>
+                        <i class="icon-interface-right"></i>
+                        Befestigungs- &amp; Montagetechnik
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Brandschutz/1601AC02_28276880.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_03">
+                        <i data-cyid="cyid28276880" class="icon-cyid28276880"></i>
+                        <i class="icon-interface-right"></i>
+                        Brandschutz
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Duebeltechnik/1601AC02_3501316.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_04">
+                        <i data-cyid="cyid3501316" class="icon-cyid3501316"></i>
+                        <i class="icon-interface-right"></i>
+                        Dübeltechnik
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Materialbearbeitung/1601AC02_3501329.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_05">
+                        <i data-cyid="cyid3501329" class="icon-cyid3501329"></i>
+                        <i class="icon-interface-right"></i>
+                        Materialbearbeitung
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Chemisch-Technische-Produkte/1601AC02_3501531.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_06">
+                        <i data-cyid="cyid3501531" class="icon-cyid3501531"></i>
+                        <i class="icon-interface-right"></i>
+                        Chemisch-Technische Produkte
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Handwerkzeuge/1601AC02_3501417.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_07">
+                        <i data-cyid="cyid3501417" class="icon-cyid3501417"></i>
+                        <i class="icon-interface-right"></i>
+                        Handwerkzeuge
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Mess-Spannwerkzeuge/1601AC02_3501489.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_08">
+                        <i data-cyid="cyid3501489" class="icon-cyid3501489"></i>
+                        <i class="icon-interface-right"></i>
+                        Mess- &amp; Spannwerkzeuge
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Diamanttechnik/1601AC02_3501500.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_09">
+                        <i data-cyid="cyid3501500" class="icon-cyid3501500"></i>
+                        <i class="icon-interface-right"></i>
+                        Diamanttechnik
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Schweiss-Loettechnik/1601AC02_3501514.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_10">
+                        <i data-cyid="cyid3501514" class="icon-cyid3501514"></i>
+                        <i class="icon-interface-right"></i>
+                        Schweiß- &amp; Löttechnik
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Arbeitsschutz/1601AC02_3501520.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_11">
+                        <i data-cyid="cyid3501520" class="icon-cyid3501520"></i>
+                        <i class="icon-interface-right"></i>
+                        Arbeitsschutz
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Elektroinstallation/1601AC02_3501546.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_12">
+                        <i data-cyid="cyid3501546" class="icon-cyid3501546"></i>
+                        <i class="icon-interface-right"></i>
+                        Elektroinstallation
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Sanitaerinstallation/1601AC02_3501564.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_13">
+                        <i data-cyid="cyid3501564" class="icon-cyid3501564"></i>
+                        <i class="icon-interface-right"></i>
+                        Sanitärinstallation
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Dach-Holzbau/1601AC02_14959716.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_14">
+                        <i data-cyid="cyid14959716" class="icon-cyid14959716"></i>
+                        <i class="icon-interface-right"></i>
+                        Dach- &amp; Holzbau
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Baubedarf/1601AC02_3501809.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_15">
+                        <i data-cyid="cyid3501809" class="icon-cyid3501809"></i>
+                        <i class="icon-interface-right"></i>
+                        Baubedarf
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Fahrzeugausstattung/1601AC02_3501589.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_16">
+                        <i data-cyid="cyid3501589" class="icon-cyid3501589"></i>
+                        <i class="icon-interface-right"></i>
+                        Fahrzeugausstattung
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Werkstattausstattung/1601AC02_34613329.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_17">
+                        <i data-cyid="cyid34613329" class="icon-cyid34613329"></i>
+                        <i class="icon-interface-right"></i>
+                        Werkstattausstattung
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Sortimente/1601AC02_3501602.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_18">
+                        <i data-cyid="cyid3501602" class="icon-cyid3501602"></i>
+                        <i class="icon-interface-right"></i>
+                        Sortimente
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Elektrowerkzeuge-Maschinen/1601AC02_3501622.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_19">
+                        <i data-cyid="cyid3501622" class="icon-cyid3501622"></i>
+                        <i class="icon-interface-right"></i>
+                        Elektrowerkzeuge &amp; Maschinen
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Beschlaege/1601AC02_3501647.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_20">
+                        <i data-cyid="cyid3501647" class="icon-cyid3501647"></i>
+                        <i class="icon-interface-right"></i>
+                        Beschläge
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Betriebseinrichtungen/1601AC02_23069284.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_21">
+                        <i data-cyid="cyid23069284" class="icon-cyid23069284"></i>
+                        <i class="icon-interface-right"></i>
+                        Betriebseinrichtungen
+                    </a>
+                </li>
+                <li class="">
+                    <a href="https://shop.recanorm.de/Gesamtkatalog/Aktionen/1601AC02_3501817.cyid/1601.cgid/de/DE/EUR/"
+                        class="bm-catalog bm-nav_catalog_22">
+                        <i data-cyid="cyid3501817" class="icon-cyid3501817"></i>
+                        <i class="icon-interface-right"></i>
+                        Aktionen
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </li>
+</ul>
+</div>`
