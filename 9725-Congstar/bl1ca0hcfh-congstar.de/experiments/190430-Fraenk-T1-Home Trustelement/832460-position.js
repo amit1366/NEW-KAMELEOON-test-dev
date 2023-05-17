8f768943de3ddf102ed3c131563b8dc4d28e92a7
Kshitij <kshitij@brillmark.com>
2023-05-17 12:45:14 +0530

    update

--- 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190430-Fraenk-T1-Home Trustelement/832460-position.js
+++ 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190430-Fraenk-T1-Home Trustelement/832460-position.js
@@ -1,13 +1,9 @@
-import { common } from "./common";
-
-const bm_strip= `<div class="bm_strip_wrap">fraenk - eine Marke der Telekom Deutschland GmbH</div>`;
-
 function init() {
 
     let homePage = 'https://fraenk.de/';
     let holDirFraenk = 'https://fraenk.de/hol-dir-fraenk/';
 
-    if(window.location.href === homePage){
+    if(window.location.href === homePage || window.location.href.indexOf(homePage+'?') > -1){
         // add #brand at the end of the url 
         window.location.href = homePage+'#brand';
     }
@@ -16,21 +12,7 @@ function init() {
         window.location.href = holDirFraenk+'#brand';
     }
 
-    if(window.location.href.indexOf('#brand') > -1 || window.location.href.indexOf('#brand') > -1){
-
-        // insert element
-        Kameleoon.API.Utils.querySelectorAll('body')[0].classList.add('bm_fraenk_t1');
-        var interval =  setInterval(function(){
-            if(Kameleoon.API.Utils.querySelectorAll('.bm_strip_wrap')[0]) return
-            Kameleoon.API.Utils.querySelectorAll('.tariff-pricing__addition')[0].insertAdjacentHTML('beforebegin', bm_strip);
-        }, 50);
-        
-        setTimeout(function(){
-            clearInterval(interval)
-        }, 5000); 
-    }
 };
-common()
 Kameleoon.API.Core.runWhenElementPresent('.tariff-pricing__addition', () => {
     init();
 
--- 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190430-Fraenk-T1-Home Trustelement/common.js
+++ 9725-Congstar/bl1ca0hcfh-congstar.de/experiments/190430-Fraenk-T1-Home Trustelement/common.js
@@ -1,106 +1,91 @@
-import { goals } from './goals';
-export const common = () =>{
-Kameleoon.API.Core.runWhenElementPresent('.tariff-pricing__addition', () => {
+import {goals} from './goals'
+Kameleoon.API.Core.runWhenElementPresent('.tariff-pricing__addition, .app-platform-tile-mobile, .app-platform-tile', () => {
 
-    Kameleoon.API.Utils.querySelectorAll('.download-app__button-wrapper [alt*="Android App"]').forEach(function(el){
-        el.closest('a').classList.add('bm-playStore');
-    })
-    Kameleoon.API.Utils.querySelectorAll('.download-app__button-wrapper [alt*="iOS App"]').forEach(function(el){
-        el.closest('a').classList.add('bm-isoApp');
-    })
-    Kameleoon.API.Utils.querySelectorAll('.app-platform-list__grid [alt*="Android App"]')[0].closest('.app-platform-tile-mobile' || '.app-platform-tile').classList.add('bm-lower-playStore');
-    Kameleoon.API.Utils.querySelectorAll('.app-platform-list__grid [alt*="iOS App"]')[0].closest('.app-platform-tile-mobile' || '.app-platform-tile').classList.add('bm-lower-iosApp');
-    Kameleoon.API.Utils.querySelectorAll('.awards__link-list .link--light').forEach(function(el){
-        if(el.textContent.trim(' ') == 'App-Download'){
-            el.classList.add('bm-footer-appDownload')
-        }
-    })
+        Kameleoon.API.Utils.querySelectorAll('.download-app__button-wrapper [alt*="Android App"]').forEach((el) => {
+            el.closest('a').classList.add('bm-playStore');
+        });
+        Kameleoon.API.Utils.querySelectorAll('.download-app__button-wrapper [alt*="iOS App"]').forEach((el) => {
+            el.closest('a').classList.add('bm-isoApp');
+        });
 
-    Kameleoon.API.Utils.addUniversalClickListener(document, ({ target }) => {
-        let playStoreCta = target.closest('.bm-playStore');
-        if(playStoreCta){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Header Click Google Play']);
-            console.log('[GG] Header Click Google Play');
-        }
+        Kameleoon.API.Utils.querySelectorAll('.awards__link-list .link--light').forEach((el) => {
+            if (el.textContent.trim(' ') == 'App-Download') {
+                el.classList.add('bm-footer-appDownload');
+            }
+        });
 
-        let appleStoreCta = target.closest('.bm-isoApp');
-        if(appleStoreCta){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Header Click App Store']);
-            console.log('[GG] Header Click App Store');
-        }
+        Kameleoon.API.Utils.addUniversalClickListener(document, ({ target }) => {
+            const playStoreCta = target.closest('.bm-playStore');
+            if (playStoreCta) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Header Click Google Play']);
+            }
 
-        let LowerplayStoreCta = target.closest('.bm-lower-playStore');
-        if(LowerplayStoreCta){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Lower Click Google Play']);
-            console.log('[GG] Lower Click Google Play');
-        }
+            const appleStoreCta = target.closest('.bm-isoApp');
+            if (appleStoreCta) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Header Click App Store']);
+            }
 
-        let LowerappleStoreCta = target.closest('.bm-lower-iosApp')
-        if(LowerappleStoreCta){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Lower Click App Store']);
-            console.log('[GG] Lower Click App Store');
-        }
+            const LowerplayStoreCta = target.closest('.app-platform-list a:first-child');
+            if (LowerplayStoreCta) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Lower Click Google Play']);
+            }
 
-        let appDownloadFooter = target.closest('.bm-footer-appDownload')
-        if(appDownloadFooter){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click App Download Footer']);
-            console.log('[GG] Click App Download Footer');
-        }
+            const LowerappleStoreCta = target.closest('.app-platform-list a:first-child + a');
+            if (LowerappleStoreCta) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Lower Click App Store']);
+            }
 
-        let Jetzt = target.closest('.tariff-pricing__content-wrapper .tariff-pricing__cta')
-        if(Jetzt){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click Jetzt loslegen']);
-            console.log('[GG] Click Jetzt loslegen');
-        }
+            const appDownloadFooter = target.closest('.bm-footer-appDownload');
+            if (appDownloadFooter) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click App Download Footer']);
+            }
 
-        let nav = target.closest('.navigation__group .link')
-        let flayoutNav = target.closest('.flyout__link-list .link')
-        let flayoutNavApp = target.closest('.flyout__menu  .download-app a')
-        if(nav || flayoutNav || flayoutNavApp){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click Navi']);
-            console.log('[GG] Click Navi');
-        }
+            const Jetzt = target.closest('.tariff-pricing__content-wrapper .tariff-pricing__cta');
+            if (Jetzt) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click Jetzt loslegen']);
+            }
 
-        let footerLinks = target.closest('.footer .awards__link-list .awards__link-item a')
-        let footerSocial = target.closest('.footer .footer__link-wrapper .social-media .social-media__item')
-        let footerimpressum = target.closest('.footer .footer__link-wrapper .footer__link-list .footer__link-item [href*="/impressum/"]')
-        let footeragb = target.closest('.footer .footer__link-wrapper .footer__link-list .footer__link-item [href*="/agb/"]')
-        let footerVerbraucherinformationen = target.closest('.footer .footer__link-wrapper .footer__link-list .footer__link-item [href*="/Verbraucherinformationen"]')
-        if(footerLinks || footerSocial || footerimpressum || footeragb || footerVerbraucherinformationen){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click Footer']);
-            console.log('[GG] Click Footer');
-        }
+            const nav = target.closest('.navigation__group .link');
+            const flayoutNav = target.closest('.flyout__link-list .link');
+            const flayoutNavApp = target.closest('.flyout__menu  .download-app a');
+            const mobFlayoutNavApp = target.closest('.navigation__link-list .navigation__link');
+            if (nav || flayoutNav || flayoutNavApp || mobFlayoutNavApp) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click Navi']);
+            }
 
-        let videoContainer = target.closest('.video-section .video-section__teaser .video-section__content')
-        if(videoContainer){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click Video']);
-            console.log('[GG] Click Video');
-        }
+            const footerLinks = target.closest('.footer .awards__link-list .awards__link-item a');
+            const footerSocial = target.closest('.footer .footer__link-wrapper .social-media .social-media__item');
+            const footerimpressum = target.closest('.footer .footer__link-wrapper .footer__link-list .footer__link-item [href*="/impressum/"]');
+            const footeragb = target.closest('.footer .footer__link-wrapper .footer__link-list .footer__link-item [href*="/agb/"]');
+            const footerVerbraucherinformationen = target.closest('.footer .footer__link-wrapper .footer__link-list .footer__link-item [href*="/Verbraucherinformationen"]');
+            // eslint-disable-next-line max-len
+            if (footerLinks || footerSocial || footerimpressum || footeragb || footerVerbraucherinformationen) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click Footer']);
+            }
 
-        let mehrInfos = target.closest('.gruende-fraenk-tiles__cardlet .cardlet__wrapper .gruende-fraenk-tiles__link')
-        if(mehrInfos){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click Mehr Infos']);
-            console.log('[GG] Click Mehr Infos');
-        }
+            const videoContainer = target.closest('.video-section .video-section__teaser .video-section__content');
+            if (videoContainer) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click Video']);
+            }
 
-        let chatIcon = target.closest('.helpButton')
-        if(chatIcon){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click Chatbubble']);
-            console.log('[GG] Click Chatbubble');
-        }
+            const mehrInfos = target.closest('.gruende-fraenk-tiles__cardlet .cardlet__wrapper .gruende-fraenk-tiles__link');
+            if (mehrInfos) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click Mehr Infos']);
+            }
 
-        let friends = target.closest('.paragraph [href*="/fraenk-for-friends/"]')
-        if(friends){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Click 4 friends']);
-            console.log('[GG] Click 4 friends');
-        }
-        let mobClick = target.closest('.smart-banner .smart-banner__button')
-        if(mobClick){
-            // Kameleoon.API.Goals.processConversion(goals['[GG] Mobile Click Download']);
-            console.log('[GG] Mobile Click Download');
-        }
-    });
+            const chatIcon = target.closest('.helpButton');
+            if (chatIcon) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click Chatbubble']);
+            }
 
-   
+            const friends = target.closest('.paragraph [href*="/fraenk-for-friends/"]');
+            if (friends) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Click 4 friends']);
+            }
+            const mobClick = target.closest('.smart-banner .smart-banner__button');
+            if (mobClick) {
+                Kameleoon.API.Goals.processConversion(goals['[GG] Mobile Click Download']);
+            }
+        });
+
 }, 200);
-}
